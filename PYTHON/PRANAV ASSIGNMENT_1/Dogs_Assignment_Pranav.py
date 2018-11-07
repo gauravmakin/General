f6a6dd8f5818555227b2651e8c88b802c0deb9c6
Gaurav <makingaurav21@gmail.com>
2018-11-07 07:13:50 -0800

    errors

--- PYTHON/PRANAV ASSIGNMENT_1/Dogs_Assignment_Pranav.py
+++ PYTHON/PRANAV ASSIGNMENT_1/Dogs_Assignment_Pranav.py
@@ -8,19 +8,25 @@ def menu():     # This function is for the menu, which displays the 2 possible o
         cardsnumber = int(input("Please enter the number of cards you would like to play with :")) # I am creating a new variable for the cardsnumber which is being stored as an integer
 
         mod = cardsnumber % 2 # I am using this to check if the number entered by the user is an even number or an odd number. It will use a percentage comparison with 2.
-        if cardsnumber == 0 and cardsnumber < 4 or cardsnumber > 30: # I am using this IF statement to see that if the user has entered an even number but the number is not entered in range
-            print("Please enter a number that is between 4 and 30") # This will tell th user to enter a number between range
-            menu()  #This will return the user to the menu
+        # if ((mod == 0) and (cardsnumber < 4 or cardsnumber > 30)): # I am using this IF statement to see that if the user has entered an even number but the number is not entered in range
+        #     print("Please enter a number that is between 4 and 30") # This will tell th user to enter a number between range
+        #     menu()  #This will return the user to the menu
 
-        elif cardsnumber > 0 and cardsnumber < 4 or cardsnumber > 30:#This elif statement will work if the number entered is within range but is not an even number  
-            print("Please enter a number that is even and is between 4 and 30") # This will now tell the user that the number entered is within range but is not an even number and the user needs an even number
-            menu() #This will return the user to the menu
+        # elif ((mod > 0) and (cardsnumber < 4 or cardsnumber > 30)):#This elif statement will work if the number entered is within range but is not an even number  
+        #     print("Please enter a number that is even and is between 4 and 30") # This will now tell the user that the number entered is within range but is not an even number and the user needs an even number
+        #     menu() #This will return the user to the menu
 
 
-        else:  #This else statement will work if the number entered is an even number and is within range
-            print("Number is even and is within the limits, Starting Game...") #This will tell the user that the number is entered is completely valid and now the cards are about to generate      
-            
-            return cardsnumber
+        # else:  #This else statement will work if the number entered is an even number and is within range
+        #     print("Number is even and is within the limits, Starting Game...") #This will tell the user that the number is entered is completely valid and now the cards are about to generate      
+        #     return int(cardsnumber)
+
+        if (cardsnumber >= 4 and cardsnumber <= 30):
+            if mod:
+                return int(cardsnumber)
+            else:
+                return menu()
+
             # f = open('dogs.txt','r')
             # dogs = f.read().splitlines()    # Read lines from file into a list 
             # #print(f.read())
@@ -74,13 +80,13 @@ def menu():     # This function is for the menu, which displays the 2 possible o
 #            else:
 #                  ("Value chosen is not valid, please try again")
 
-    elif choice == "2":
+    else: #choice == "2":
         print ("You are now exiting the program, Thanks for playing!")
         exit
 
-    else:
-        print("This is not a valid option, Please try again")
-        menu()
+    #else:
+    #    print("This is not a valid option, Please try again")
+    #   return menu()
 
 
 def read_file():                        # Function to read file
@@ -89,11 +95,35 @@ def read_file():                        # Function to read file
     f.close()
     return dogs
 
+def check_number(cardsnumber):
+    mod = cardsnumber % 2 # I am using this to check if the number entered by the user is an even number or an odd number. It will use a percentage comparison with 2.
+    if (mod or cardsnumber > 4 or cardsnumber < 30):
+        return cardsnumber
+    else:
+        print('Goodbye')
+        pass
+
+    # if cardsnumber == 0 and cardsnumber < 4 or cardsnumber > 30: # I am using this IF statement to see that if the user has entered an even number but the number is not entered in range
+    #         print("Please enter a number that is between 4 and 30") # This will tell th user to enter a number between range
+    #         #menu()  #This will return the user to the menu
+
+    # elif cardsnumber > 0 and cardsnumber < 4 or cardsnumber > 30:#This elif statement will work if the number entered is within range but is not an even number  
+    #         print("Please enter a number that is even and is between 4 and 30") # This will now tell the user that the number entered is within range but is not an even number and the user needs an even number
+    #         #menu() #This will return the user to the menu
+
+
+    # else:  #This else statement will work if the number entered is an even number and is within range
+    #         print("Number is even and is within the limits, Starting Game...") #This will tell the user that the number is entered is completely valid and now the cards are about to generate
+            
+
+
+
 def create_category_values(cardsnumber):    # Function to randomly generate values for the 4 categories for each card
     i = 0
     category_values = []   # List to store values for each category
-            
-    while i <= cardsnumber-1:
+    cardsnumber = int(cardsnumber)
+    
+    while i < cardsnumber:
         temp1 = []                      # Temporary list to store random generate values for each category
         temp1.append(randint(1,5))      # Value for Exercise
         temp1.append(randint(1,100))    # Value for Intelligence
