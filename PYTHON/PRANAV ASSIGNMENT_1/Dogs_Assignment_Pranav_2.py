89109c209afdf09b0c0743ae5aea99232fecd844
Gaurav <makingaurav21@gmail.com>
2018-11-12 21:25:35 -0800

    wow

--- PYTHON/PRANAV ASSIGNMENT_1/Dogs_Assignment_Pranav_2.py
+++ PYTHON/PRANAV ASSIGNMENT_1/Dogs_Assignment_Pranav_2.py
@@ -1,66 +1,283 @@
-from random import randint
 
-def menu():     # This function is for the menu, which displays the 2 possible options for the user to play or quit. 
-    print("Welcome to Celebrity Dogs: \n 1. Play the Game \n 2. Quit") # This displays the 2 possible options for the user to choose from.
-    choice = input() # I am creating a variable for the user choice, whether it is 1, 2 or some other option. It is taking the choice as an input
+import time
+import random 
 
-    if choice == "1": # The statement is looking, if the user chooses the first option and wants to play the game
-        cardsnumber = int(input("Please enter the number of cards you would like to play with :")) # I am creating a new variable for the cardsnumber which is being stored as an integer
-
-        mod = cardsnumber % 2 # I am using this to check if the number entered by the user is an even number or an odd number. It will use a percentage comparison with 2.
-        if cardsnumber == 0 and cardsnumber < 4 or cardsnumber > 30: # I am using this IF statement to see that if the user has entered an even number but the number is not entered in range
-            print("Please enter a number that is between 4 and 30") # This will tell th user to enter a number between range
-            menu()  #This will return the user to the menu
-
-        elif cardsnumber > 0 and cardsnumber < 4 or cardsnumber > 30:#This elif statement will work if the number entered is within range but is not an even number  
-            print("Please enter a number that is even and is between 4 and 30") # This will now tell the user that the number entered is within range but is not an even number and the user needs an even number
-            menu() #This will return the user to the menu
+menu = """
 
 
-        else:  #This else statement will work if the number entered is an even number and is within range
-            print("Number is even and is within the limits, Starting Game...") #This will tell the user that the number is entered is completely valid and now the cards are about to generate      
-                  
-            f = open('dogs.txt','r')
-            dogs = f.read().splitlines()    # Read lines from file into a list 
-            #print(f.read())
-            print(dogs)
-            print(type(dogs))
+To begin, Please choose one of the options below:
+1 - Play the Game
+2 - Exit
+Option: """
+DogPic="""
+         __.                                              
+        .-".'                      .--.            _..._    
+      .' .'                     .'    \       .-""  __ ""-. 
+     /  /                     .'       : --..:__.-""  ""-. \
+    :  :                     /         ;.d$$    sbp_.-""-:_:
+    ;  :                    : ._       :P .-.   ,"TP        
+    :   \                    \  T--...-; : d$b  :d$b        
+     \   `.                   \  `..'    ; $ $  ;$ $        
+      `.   "-.                 ).        : T$P  :T$P        
+        \..---^..             /           `-'    `._`._     
+       .'        "-.       .-"                     T$$$b    
+      /             "-._.-"               ._        '^' ;   
+     :                                    \.`.         /    
+     ;                                -.   \`."-._.-'-'     
+    :                                 .'\   \ \ \ \   Woof!        
+    ;  ;                             /:  \   \ \ . ;  Hi! Welcome to Celebrity Dogs!       
+   ;    \        ;                     ;    "-._:  ;         
+  :      `.      :                     :         \/         
+  ;       /"-.    ;                    :                    
+ :       /    "-. :                  : ;                    
+ :     .'        T-;                 ; ;        
+ ;    :          ; ;                /  :        
+ ;    ;          : :              .'    ;       
+:    :            ;:         _..-"\     :       
+:     \           : ;       /      \     ;      
+;    . '.         '-;      /        ;    :      
+;  \  ; :           :     :         :    '-.      
+'.._L.:-'           :     ;          ;    . `. 
+                     ;    :          :  \  ; :  
+                     :    '-..       '.._L.:-'  
+                      ;     , `.                
+                      :   \  ; :                
+                      '..__L.:-'
 
-            category = ['Exercise', 'Intelligence','Friendliness','Drool']  # Pre defined categories in a list
-            
-            i = 0
-            category_values = []   # List to store values for each category
-            
-            while i <= cardsnumber-1:
-                temp1 = []                      # Temporary list to store random generate values for each category
-                temp1.append(randint(1,5))      # Value for Exercise
-                temp1.append(randint(1,100))    # Value for Intelligence
-                temp1.append(randint(1,10))     # Value for Friendliness
-                temp1.append(randint(1,10))     # Value for Drool
-                category_values.append(temp1)   # Append random values into another list. This will give you list of lists
-                i = i + 1                       # Increment so you only generate values only for the number of cards the user wants to play with
-            print(category_values)
-            print("1st Value is Exercise\n2nd value is Intelligence \n3rd Value is Friendliness \n4th Value is Drool")
-            value = (input("Please choose one of the categories you would like to start with and enter 1,2,3 or 4 based on their position")
-            if value in category[][]:
-                  print("Thank you for choosing")
-                  print("Comparing against the Computer...")
-            else:
-                  ("Value chosen is not valid, please try again")
-            
-            
-            
-                        
+
+"""
+print(DogPic)
 
 
-    elif choice == "2":
-        print ("You are now exiting the program, Thanks for playing!")
-        exit
+category_chosen = """
 
-    else:
-        print("This is not a valid option, Please try again")
-        menu()
+Please select a category you would like to comapare and use against the computer in this round:
+1 - Intelligence
+2 - Exercise
+3 - Friendliness
+4 - Drool
+"""
 
-menu()
+ 
+
+           
+def cardsnumber():
+    while True:
+        print(("Please enter the number of cards you would like to play with"))
+        global cardsnumber
+        cardsnumber=int(input("Number of cards you would like to play with against the computer: "))
+        time.sleep(0.5)
+ 
+        mod = cardsnumber % 2
+        if mod > 0 and cardsnumber < 4 or cardsnumber > 30:
+            print("Checking")
+            print("...")
+            time.sleep(0.5)
+            print("Please enter a number that is even and is between 4 and 30")
+
+        elif mod > 0 and cardsnumber > 4 and cardsnumber < 30:
+            print("Checking")
+            print("...")
+            time.sleep(0.5)
+            print("Number entered is within range but please enter an even number")
+
+        elif mod == 0 and cardsnumber < 4 or cardsnumber > 30:
+            print("Checking")
+            print("...")
+            time.sleep(0.5)
+            print(" Number entered is even but please enter a number between 4 and 30")
+
+        else:
+            return cardsnumber        
+ 
+def dog_cards ():
+    global dogs
+    with open('dogs.txt') as dogs:
+        dogs = dogs.read().splitlines()
+        dogs = [line.split(",") for line in dogs]
+
+    return dogs
     
+
+
+def game_against_computer():
     
+    playercards = cardsnumber/2
+    computercards = cardsnumber/2
+
+    while True:
+        player_intelligence = random.randint(1,100) 
+        player_exercise = random.randint(1,5)
+        player_friendliness = random.randint(1,10)
+        player_drool = random.randint(1,10)
+   
+        computer_intelligence = random.randint(1,100)
+        computer_exercise = random.randint(1,5)
+        computer_friendliness = random.randint(1,10)
+        computer_drool = random.randint(1,10)
+        print("Currenty you have :",playercards,"cards in your deck")
+        time.sleep(1)
+        print("The computer has :",computercards,"cards in their deck")
+        time.sleep(1)
+        print("Your dog's name is:")
+        print(random.choice(dogs))
+        time.sleep(0.5)
+        print(" 1 - The Intelligence value is : ",player_intelligence)
+        time.sleep(1)
+        print(" 2 - The Exercise value is: ",player_exercise)
+        time.sleep(1)
+        print(" 3 - The Friendliness value is: ",player_friendliness)
+        time.sleep(1)
+        print(" 4 - The Drool value is: ",player_drool)
+        time.sleep(1)
+        category = int(input("Please Select a category that you would like to compare against the computer: \n 1 for Intelligence \n 2 for Exercise \n 3 for Friendliness \n 4 for Drool \n Please choose: "))
+        time.sleep(0.5)
+        
+            
+        if category == 1:
+            print("Intelligence Value has been chosen!")
+            time.sleep(0.5)
+            print("Looking at the Computer's card...")
+            time.sleep(1)
+            print("Your intelligence value is : ", player_intelligence)
+            time.sleep(0.5)
+            
+
+            print("The computer's dog was: ")
+            print(random.choice(dogs))
+            print(" Computer's intelligence value is : ",computer_intelligence)
+            time.sleep(0.5)
+            if computer_intelligence > player_intelligence:
+                print("Computer wins this round!")
+                time.sleep(0.5)
+                playercards = playercards - 1
+                computercards = computercards + 1
+                time.sleep(0.5)
+                dog_cards()
+                y = 1
+            elif computer_intelligence <= player_intelligence:               
+                print("Congratulations!, you have won this round.")
+                time.sleep(0.5)
+                playercards = playercards + 1
+                computercards = computercards - 1
+                dog_cards()
+                y = 1
+        elif category == 2:
+            print("Exercise value has been chosen!")
+            print("Looking at the Computer's card...")
+            time.sleep(1)
+            print("Your exercise value is : ",player_exercise)
+            print("The computer's dog was: ")
+            print(random.choice(dogs))
+            print("Computer's exercise value is : ",computer_exercise)
+            time.sleep(0.5)
+            
+            if computer_exercise > player_exercise:                 
+                print("Computer wins this round!")
+                time.sleep(0.5)
+                playercards = playercards - 1
+                computercards = computercards + 1
+                dog_cards()
+                y = 1
+            elif computer_exercise <= player_exercise:
+                print("Congratulations!, you have won this round")
+                time.sleep(0.5)
+                playercards = playercards + 1
+                computercards = computercards - 1
+                dog_cards()
+                y = 1
+        elif category == 3:
+            print("Friendliness value has been chosen!")
+            print("Looking at the Computer's card...")
+            time.sleep(1)
+            print("Your friendliness value is : ",player_friendliness)
+            print("The computer's dog was: ")
+            print(random.choice(dogs))
+            print("Computer's friendliness value is : ",computer_friendliness)
+            time.sleep(0.5)
+            
+             
+            if computer_friendliness > player_friendliness:
+                print("Computer wins this round!")
+                time.sleep(0.5)
+                playercards = playercards- 1
+                computercards = computercards + 1
+                dog_cards()
+                y = 1
+            elif computer_friendliness <= player_friendliness:
+                print("Congratulations!,you have won this round.")
+                time.sleep(0.5)
+                playercards = playercards + 1
+                computercards = computercards - 1
+                dog_cards()
+                y = 1
+        elif category == 4:
+            print("Drool value has been chosen!")
+            print("Looking at the Computer's card...")
+            time.sleep(1)
+            print("Your drool value is : ",player_drool)
+            print("The computer's dog was: ")
+            print(random.choice(dogs))
+            print("Computer's drool value is : ",computer_drool)
+            time.sleep(0.5)
+            
+            if computer_drool > player_drool:
+                print("Congratulations you have won this round!")
+                time.sleep(0.5)
+                playercards = playercards + 1
+                computercards = computercards - 1
+                dog_cards()
+                y = 1
+            elif computer_drool <= player_drool:               
+                print("Computer wins this round!")
+                time.sleep(0.5)
+                playercards = playercards - 1
+                computercards = computercards + 1
+                dog_cards()
+                y = 1
+        if playercards == cardsnumber:
+            print("Congratulations! you have won the game!")
+            time.sleep(0.5)
+            print("The program is ending...")
+            time.sleep(0.5)
+            print("Thank you for playing")
+            time.sleep(0.5)
+            exit(0)
+            
+        elif computercards == cardsnumber:
+            print(" The Computer has won the game, Try again next time!")
+            time.sleep(0.5)
+            print("Thank you for playing! Please come back! ")
+            time.sleep(0.5)
+            print("The game is closing")
+            time.sleep(0.5)
+            print("...")
+            time.sleep(0.5)
+            exit(0)
+            time.sleep(0.5)
+
+
+
+        
+def menu_final():
+    while True:
+        userchoice = int(input(menu)) 
+        if userchoice == 2:
+            print("Thank you for choosing Celebrity Dogs! Come play later!")
+            time.sleep(0.5)
+            exit(0)
+        elif userchoice == 1:
+            cardsnumber()
+            dog_cards()
+            game_against_computer()
+        else:
+            print("This not a valid option, please try again !")
+            time.sleep(0.5)
+
+
+while True:
+    menu_final()
+
+            
+
+
